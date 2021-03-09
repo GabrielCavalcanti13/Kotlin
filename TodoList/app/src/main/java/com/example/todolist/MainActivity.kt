20ea57df67d91d6b690e41bf8da04a93f890136f
gabrielcavalcanti13 <gcm2@cin.ufpe.br>
2021-03-08 22:37:45 -0300

    creating functions for add and delete buttons

--- TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItens.adapter = todoAdapter
+        rvTodoItens.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {  }
     }
 }
--- TodoList/app/src/main/java/com/example/todolist/Todo.kt
+++ TodoList/app/src/main/java/com/example/todolist/Todo.kt
@@ -1,6 +1,6 @@
 package com.example.todolist
 
 data class Todo (
-    val title: String,
-    val isChecked: Boolean = false
+        val title: String,
+        var isChecked: Boolean = false
 )
